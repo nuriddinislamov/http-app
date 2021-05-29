d50c5d427a263eae49ffe511a451cba379d91627
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-05-29 09:12:54 +0500

    Improve indentation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
+/** @format */
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
